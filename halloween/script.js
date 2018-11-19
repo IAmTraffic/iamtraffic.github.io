f6d052f6420d410f0b460fade2321db92bf91612
Ivy O'Neal-Odom <riptomimura@gmail.com>
2018-11-19 15:18:52 -0500

    Test halloween

--- halloween/script.js
+++ halloween/script.js
@@ -4,13 +4,18 @@ $(document).ready(function(){
 	var currentMonth = date.getMonth();
 	var currentYear = date.getYear();
 
+	var display = $("#countdown");
+
 	console.log(currentDate + ", " + currentMonth + ", " + currentYear);
 
-	//Is it halloween?
-
+	if(currentDate == 31 && currentMonth == 9){	//It is halloween!
+		display.innerHTML = "It is halloween!";
+	}else{
+		display.innerHTML = "IT is not halloween";
 		//Is it before Halloween of this year?
 
 			//Get halloween of this year date
 
 		//If no, get halloween of next year
+	}
 });
