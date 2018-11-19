ba4366961f5a54907ec9ce07f2661b4ebd0fe751
Ivy O'Neal-Odom <riptomimura@gmail.com>
2018-11-19 15:07:18 -0500

    Work pls?

--- halloween/script.js
+++ halloween/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 	console.log("asdljfalsdj");
+
+	//Is it halloween?
+
+		//Is it before Halloween of this year?
+
+			//Get halloween of this year date
+
+		//If no, get halloween of next year
 })
