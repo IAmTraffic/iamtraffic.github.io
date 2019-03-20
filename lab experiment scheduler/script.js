af0dfc17d812dd493bb1cf80a043a58eb569e06e
Ivy O'Neal-Odom <ivyoo@protonmail.com>
2019-03-20 14:40:53 -0400

    Update script.js

--- lab experiment scheduler/script.js
+++ lab experiment scheduler/script.js
@@ -1,7 +1,7 @@
 var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];$(document).ready(function(){
 
 $("#calculateButton").click(function(){
-		var dayOfWeekNum = daysOfWeek.indexOf("Wednesday");
+		var dayOfWeekNum = daysOfWeek.indexOf($("#dayInput").value);
 		console.log(dayOfWeekNum);
 	});
 });
